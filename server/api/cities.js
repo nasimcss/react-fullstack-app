b45891a9dc9110a6aab46a260e03423b05956b39
nasim <admin@naziim.co.uk>
2019-11-16 20:20:19 +0500

    Code Completed Front End

--- server/api/cities.js
+++ server/api/cities.js
@@ -3,16 +3,17 @@ var Cities = require('../models/cities');
 
 var router = express.Router();
 
-router.get('/', function(req, res){
-    Cities.retrieveAll(function(err, cities){
+router.get('/', (req, res) => {
+    Cities.retrieveAll((err, cities) => {
         if(err)
             return res.json(err);
         return res.json(cities);
     });
 });
 
-router.post('/', function(req, res){
-    Cities.insert(city, function (err, result) {
+router.post('/', (req, res) => {
+    var city = req.body.city;
+    Cities.insert(city, (err, result) => {
         if(err)
             return res.json(err);
         return res.json(result);
